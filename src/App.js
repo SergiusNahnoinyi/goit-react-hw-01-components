82141a7754de5a0f7a686bd8168583eb9e174863
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2022-02-07 12:42:34 +0200

    Импортирует данные в App и указывает значение свойств обьекта

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Profile from './components/Profile';
+import user from './data/user.json';
 
 export default function App() {
   return (
     <div>
-      <Profile />
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+      />
     </div>
   );
 }
