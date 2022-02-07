63600be7dade8cd14ae2dcb6bfec20aabc5e4848
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2022-02-07 18:56:21 +0200

    Реализует рендеринг неограниченного списка друзей

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,7 +1,28 @@
+import PropTypes from 'prop-types';
 import FriendListItem from './FriendListItem';
 
 export default function FriendList({ friends }) {
   return (
-    <ul className="friend-list">{/* Довільна кіл-сть FriendListItem */}</ul>
+    <ul className="friend-list">
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          isOnline={friend.isOnline}
+          avatar={friend.avatar}
+          name={friend.name}
+        />
+      ))}
+    </ul>
   );
 }
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+};
--- src/components/FriendListItem.js
+++ src/components/FriendListItem.js
@@ -1,9 +1,18 @@
-export default function FriendListItem({ avatar, name, isOnline }) {
+import PropTypes from 'prop-types';
+
+export default function FriendListItem({ id, avatar, name, isOnline }) {
   return (
-    <li className="item">
-      <span className="status"></span>
-      <img className="avatar" src="" alt="User avatar" width="48" />
-      <p className="name"></p>
+    <li className="item" key={id}>
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
     </li>
   );
 }
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+  id: PropTypes.number,
+};
