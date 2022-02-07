f71b3408cea381299c02c20e1114e5a6b7608987
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2022-02-07 12:38:42 +0200

    Задает свойства, которые должна получать функция с файла user.json, а также переименовывает class на className

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,29 +1,25 @@
-export default function Profile() {
+export default function Profile({ avatar, username, tag, location }) {
   return (
-    <div class="profile">
-      <div class="description">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          class="avatar"
-        />
-        <p class="name">Petra Marica</p>
-        <p class="tag">@pmarica</p>
-        <p class="location">Salvador, Brasil</p>
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
       </div>
 
-      <ul class="stats">
+      <ul className="stats">
         <li>
-          <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+          <span className="label">Followers</span>
+          <span className="quantity">1000</span>
         </li>
         <li>
-          <span class="label">Views</span>
-          <span class="quantity">2000</span>
+          <span className="label">Views</span>
+          <span className="quantity">2000</span>
         </li>
         <li>
-          <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+          <span className="label">Likes</span>
+          <span className="quantity">3000</span>
         </li>
       </ul>
     </div>
