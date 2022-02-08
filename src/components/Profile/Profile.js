e86838d1474a442e4b31f0477fdfd00f9450dbda
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-08 15:02:02 +0200

    Стилизирует компонент Statistics

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -12,15 +12,15 @@ export default function Profile({ avatar, username, tag, location, stats }) {
       </div>
 
       <ul className={s.stats}>
-        <li className={s.stats__item}>
+        <li className={s.item}>
           <span className={s.label}>Followers</span>
           <span className={s.quantity}>{stats.followers}</span>
         </li>
-        <li className={s.stats__item}>
+        <li className={s.item}>
           <span className={s.label}>Views</span>
           <span className={s.quantity}>{stats.views}</span>
         </li>
-        <li className={s.stats__item}>
+        <li className={s.item}>
           <span className={s.label}>Likes</span>
           <span className={s.quantity}>{stats.likes}</span>
         </li>
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
 
 export default function Statistics({ title, stats }) {
   return (
-    <section className="statistics">
-      <h2 className="title">{title && 'Upload stats'}</h2>
-      <ul className="stat-list">
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+      <ul className={s.list}>
         {stats.map(stat => (
-          <li className="item" key={stat.id}>
-            <span className="label">{stat.label}</span>
-            <span className="percentage">{stat.percentage}</span>
+          <li
+            className={s.item}
+            key={stat.id}
+            style={{ backgroundColor: stat.color }}
+          >
+            <span className={s.label}>{stat.label}</span>
+            <span className={s.percentage}>{stat.percentage}%</span>
           </li>
         ))}
       </ul>
