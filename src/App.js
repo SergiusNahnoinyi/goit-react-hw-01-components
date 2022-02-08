5fbd1a73b0af428b089ca105f030b710f238b05e
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-08 09:18:25 +0200

    Добавляет компонент TransactionHistory и импортирует его в App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Profile from './components/Profile';
 import Statistics from './components/Statistics';
 import FriendList from './components/FriendList';
+import TransactionHistory from './components/TransactionHistory';
+
 import user from './data/user.json';
 import data from './data/data.json';
 import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 export default function App() {
   return (
@@ -17,6 +20,7 @@ export default function App() {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
